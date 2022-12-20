6e09ee14ab17b449450ef3d865dffd59fc63713d
rajpal229 <rajpalrawat229@gmail.com>
2022-12-20 11:55:28 +0530

    Added: UC3_EmailAddress

--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@
             bool flag = true;
             while(flag = true)
             {
-                Console.WriteLine("1. Enter First Name\n2. Enter Last Name");
+                Console.WriteLine("1. Enter First Name\n2. Enter Last Name\n3. Enter Email Address");
                 int commmand = Convert.ToInt32(Console.ReadLine());
                 switch (commmand)
                 {
@@ -19,6 +19,10 @@
                     case 2:
                         LastName lastname = new LastName();
                         lastname.Last();
+                        break;
+                    case 3:
+                        EmailAddress emailaddress = new EmailAddress();
+                        emailaddress.Email();
                         break;
                     default:
                         Console.WriteLine("Invalid Input");
