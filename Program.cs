3716116b02423a336c199baf323b8ed3752a28b3
rajpal229 <rajpalrawat229@gmail.com>
2022-12-20 11:36:54 +0530

    Added: UC1_FirstName

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿namespace UserRegistrationProblem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to User Registration\n1. To Enter First Name");
+            int commmand = Convert.ToInt32(Console.ReadLine());
+            switch (commmand)
+            {
+                case 1:
+                    FirstName firstname = new FirstName();
+                    firstname.First();
+                    break;
+                default:
+                    Console.WriteLine("Invalid Input");
+                    break;
+            }
+            
+        }
+    }
+}
